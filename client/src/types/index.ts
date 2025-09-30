0fbff678884a1e01f40a52bd613129039dbd5c79
DemaPy <benderdema@gmail.com>
2025-09-30 13:07:28 +0200

    feat: create types for props blocks

---
+++ client/src/types/index.ts
@@ -0,0 +1,52 @@
+type ComponentType = "blocks.hero-section" | "blocks.info-block";
+
+interface Base<
+  T extends ComponentType,
+  D extends object = Record<string, unknown>
+> {
+  id: number;
+  data?: D;
+  __component?: T;
+  documentId?: string;
+  createdAt?: string;
+  updatedAt?: string;
+  publishedAt?: string;
+}
+
+export interface LinkProps {
+  id: number;
+  label: string;
+  href: string;
+  external: boolean;
+}
+
+export interface ImageProps {
+  id: number;
+  url: string;
+  alternativeText: string | null;
+}
+
+export interface LogoProps {
+  id: number;
+  logoText: string;
+  image: ImageProps;
+}
+
+export interface HeroSectionProps extends Base<"blocks.hero-section"> {
+  heading: string;
+  theme: "green" | "orange";
+  logo: LogoProps;
+  image: ImageProps;
+  cta: LinkProps;
+}
+
+export interface InfoBlockProps extends Base<"blocks.info-block"> {
+  title: string;
+  description: string;
+  image: ImageProps;
+  cta: LinkProps;
+  theme: "green" | "orange";
+  reversed: boolean;
+}
+
+export type BlockProps = HeroSectionProps | InfoBlockProps;
