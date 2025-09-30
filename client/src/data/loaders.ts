83d8254c79d4dec3e8fd9632c20853466cb59dae
DemaPy <benderdema@gmail.com>
2025-09-30 16:02:49 +0200

    feat: add global and dynamic page loaders

--- client/src/data/loaders.ts
+++ client/src/data/loaders.ts
@@ -39,3 +39,73 @@ export async function getHomePage() {
     search: HOME_PAGE_QUERY,
   });
 }
+
+const DYNAMIC_PAGE_QUERY = (slug: string) =>
+  qs.stringify({
+    filters: {
+      slug: {
+        $eq: slug,
+      },
+    },
+    populate: {
+      body: {
+        on: {
+          "blocks.info-block": {
+            populate: {
+              image: {
+                fields: ["url", "alternativeText"],
+              },
+              cta: true,
+            },
+          },
+          "blocks.hero-section": {
+            populate: {
+              logo: {
+                populate: {
+                  image: {
+                    fields: ["url", "alternativeText"],
+                  },
+                },
+              },
+              image: {
+                fields: ["url", "alternativeText"],
+              },
+              cta: true,
+            },
+          },
+        },
+      },
+    },
+  });
+
+export async function getDynamicPage(slug: string) {
+  return fetchAPI({
+    pathname: "/api/pages/",
+    search: DYNAMIC_PAGE_QUERY(slug),
+  });
+}
+
+const GLOBAL_LAYOUT_QUERY = qs.stringify({
+  populate: {
+    header: {
+      populate: {
+        logo: {
+          populate: {
+            image: {
+              fields: ["url"],
+            },
+          },
+        },
+        cta: true,
+        navigation: true,
+      },
+    },
+  },
+});
+
+export async function getGlobalLayout() {
+  return fetchAPI({
+    pathname: "/api/global",
+    search: GLOBAL_LAYOUT_QUERY,
+  });
+}
