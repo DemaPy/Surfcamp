226a34e081d9e76dadaca998f6ff13873fdd5662
DemaPy <benderdema@gmail.com>
2025-09-30 10:54:43 +0200

    feat: generate types

--- cms/types/generated/components.d.ts
+++ cms/types/generated/components.d.ts
@@ -1,3 +1,63 @@
-/*
- * The app doesn't have any components yet.
- */
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface BlocksHeroSection extends Struct.ComponentSchema {
+  collectionName: 'components_blocks_hero_sections';
+  info: {
+    displayName: 'Hero Section';
+  };
+  attributes: {
+    cta: Schema.Attribute.Component<'element.link', false>;
+    heading: Schema.Attribute.String;
+    image: Schema.Attribute.Media<'images'> & Schema.Attribute.Required;
+    logo: Schema.Attribute.Component<'element.logo', false>;
+    theme: Schema.Attribute.Enumeration<['orange', 'green']>;
+  };
+}
+
+export interface BlocksInfoBlock extends Struct.ComponentSchema {
+  collectionName: 'components_blocks_info_blocks';
+  info: {
+    displayName: 'Info block';
+  };
+  attributes: {
+    cta: Schema.Attribute.Component<'element.link', false>;
+    description: Schema.Attribute.RichText & Schema.Attribute.Required;
+    image: Schema.Attribute.Media<'images'> & Schema.Attribute.Required;
+    theme: Schema.Attribute.Enumeration<['orange', 'green']>;
+    title: Schema.Attribute.String & Schema.Attribute.Required;
+  };
+}
+
+export interface ElementLink extends Struct.ComponentSchema {
+  collectionName: 'components_element_links';
+  info: {
+    displayName: 'Link';
+  };
+  attributes: {
+    external: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+    href: Schema.Attribute.String;
+    label: Schema.Attribute.String;
+  };
+}
+
+export interface ElementLogo extends Struct.ComponentSchema {
+  collectionName: 'components_element_logos';
+  info: {
+    displayName: 'Logo';
+  };
+  attributes: {
+    image: Schema.Attribute.Media<'images'> & Schema.Attribute.Required;
+    logoText: Schema.Attribute.String;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'blocks.hero-section': BlocksHeroSection;
+      'blocks.info-block': BlocksInfoBlock;
+      'element.link': ElementLink;
+      'element.logo': ElementLogo;
+    }
+  }
+}
