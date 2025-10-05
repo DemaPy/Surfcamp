0acf0aef50796dd3157658abf2815cf9a41dfcc7
DemaPy <benderdema@gmail.com>
2025-10-05 17:02:05 +0200

    fix: heading component props

--- client/src/components/blocks/Heading.tsx
+++ client/src/components/blocks/Heading.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import type { HeadingProps } from "@/types";
 
-export function Heading({ heading, linkId }: Readonly<HeadingProps>) {
+export function Heading({ headline, linkId }: Readonly<HeadingProps>) {
   return (
     <h3 className="article-headline" id={linkId}>
-      {heading}
+      {headline}
     </h3>
   );
 }
